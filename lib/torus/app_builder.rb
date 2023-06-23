e081e4b0ef609613128012d728e482462ee80cb2
notb0t <wilber.garcia@outlook.es>
2023-06-23 16:01:19 -0400

    feat: add configuration for default generators in the app builder

--- lib/torus/app_builder.rb
+++ lib/torus/app_builder.rb
@@ -3,5 +3,24 @@ module Torus
     def gemfile
       template "Gemfile.erb", "Gemfile"
     end
+
+    def configure_generators
+      config = <<-RUBY
+    config.generators do |generate|
+      generate.helper false
+      generate.javascripts false
+      generate.controller_specs false
+      generate.request_specs true
+      generate.routing_specs false
+      generate.stylesheets false
+      generate.test_framework :rspec
+      generate.view_specs false
+      generate.template_engine :haml
+    end
+
+      RUBY
+
+      inject_into_class "config/application.rb", "Application", config
+    end
   end
 end
--- lib/torus/generators/app.rb
+++ lib/torus/generators/app.rb
@@ -4,6 +4,19 @@ require "rails/generators/rails/app/app_generator"
 module Torus
   module Generators
     class App < Rails::Generators::AppGenerator
+      def finish_template
+        invoke :torus_custom
+        super
+      end
+
+      def torus_custom
+        invoke :setup_config
+      end
+
+      def setup_config
+        build :configure_generators
+      end
+
       protected
 
       def get_builder_class
