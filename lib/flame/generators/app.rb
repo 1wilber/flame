1126420694c44bf5fd128a9b9927ff0a024333e1
notb0t <wilber.garcia@outlook.es>
2023-07-13 01:56:58 -0400

    feat: Add rubocop-rspec and solargraph-rails
    with solargraph-rails our develop experience is very comfortable

--- lib/flame/generators/app.rb
+++ lib/flame/generators/app.rb
@@ -27,8 +27,9 @@ module Flame
         invoke "flame:vite"
 
         generate("annotate:install")
+        template ".rubocop.yml", ".rubocop.yml"
+        template ".solargraph.yml", ".solargraph.yml"
         run("bundle exec standardrb --fix-unsafely")
-        run("bundle exec haml-lint app/views -A -a")
         rails_command("db:migrate") if yes?("\nDo you want to run migrations? [y/n]")
         rails_command("db:seed") if yes?("\nDo you want to run seed? [y/n]")
         welcome_message
